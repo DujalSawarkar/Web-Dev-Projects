76d701700851db798b924e47ed8447740bb3fe22
DujalSawarkar <dujalsawar5@gmail.com>
2023-05-03 19:55:52 +0530

    added js file

---
+++ navbar/index.js
@@ -0,0 +1,13 @@
+// const mainDiv = document.querySelector(".dynamic");
+
+// const createDiv = () => {
+//   for (let i = 0; i < 100; i++) {
+//     let newDiv = document.createElement("div");
+//     newDiv.classList.add("new");
+//     mainDiv.appendChild(newDiv);
+//   }
+// };
+
+// setTimeout(() => {
+//   createDiv();
+// }, 5000);
