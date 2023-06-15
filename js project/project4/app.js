7c4e669f61cc210688ee5fa1e16ff7d0b3f2c167
DujalSawarkar <dujalsawar5@gmail.com>
2023-06-15 11:46:36 +0530

    updated js file

---
+++ js project/project4/app.js
@@ -0,0 +1,27 @@
+const upperset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerset = "abcdefghijklmnopqrstuvwxyz";
+const numberset ="1234567890";
+const symbolset ="!@#$%^&*-";
+
+const totalchar = document.getElementById("total-char")
+const upperinput = document.getElementById("UpperCase");
+const lowerinput = document.getElementById("LowerCase");
+const numberinput = document.getElementById("number");
+const symbolinput = document.getElementById("Symbol");
+const passbox = document.getElementById("pass-box")
+const button = document.querySelector(".btn");
+
+const getrandomdata = (dataset) => {
+    return dataset[Math.floor( Math.random() * dataset.length)];
+}
+
+const getpass = (password = "") => {
+if(upperinput.checked){
+    password += getrandomdata(upperset);
+}
+console.log(password);
+}
+
+button.addEventListener("click", function(){
+getpass();
+})
