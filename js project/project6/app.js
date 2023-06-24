2f570b391b6571885b16115691937e9bc42d5e0c
DujalSawarkar <dujalsawar5@gmail.com>
2023-06-25 03:38:38 +0530

    added some JS

--- js project/project6/app.js
+++ js project/project6/app.js
@@ -2,19 +2,28 @@ const giturl = "https://api.github.com/users/";
 
 
 const input = document.querySelector("#search")
-const pic = document.getElementById("pic")
+
 
 
 const geturl = async (username) => {
   const Response = await fetch(giturl+username);
   const data = await Response.json();
   console.log(data);
-
+  showdata(data);
 };
 // geturl("DujalSawarkar");
 
+const showdata = (item) =>{
+  const maindiv = document.querySelector("#main")
+  const boxdiv = document.createElement("div")
+  boxdiv.classList.add("box")
+  maindiv.appendChild(boxdiv)
+  console.log("done")
+
+}
+
 input.addEventListener(
-  "keyup",
+  "focusout",
   function(){
     geturl(input.value)
   }
