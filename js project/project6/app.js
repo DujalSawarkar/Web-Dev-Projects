99972ed76c079a25e3960262a3a863e48a06ed6b
DujalSawarkar <dujalsawar5@gmail.com>
2023-06-25 01:08:08 +0530

    updated JS

--- js project/project6/app.js
+++ js project/project6/app.js
@@ -1,1 +1,21 @@
-const giturl = "https://api.github.com/users/"
+const giturl = "https://api.github.com/users/";
+
+
+const input = document.querySelector("#search")
+const pic = document.getElementById("pic")
+
+
+const geturl = async (username) => {
+  const Response = await fetch(giturl+username);
+  const data = await Response.json();
+  console.log(data);
+
+};
+// geturl("DujalSawarkar");
+
+input.addEventListener(
+  "keyup",
+  function(){
+    geturl(input.value)
+  }
+)
