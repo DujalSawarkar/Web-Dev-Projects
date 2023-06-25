5cef18ebd0e9de5f9f5f2fd46a790c23484e8209
DujalSawarkar <dujalsawar5@gmail.com>
2023-06-26 02:18:27 +0530

    done with some js

--- js project/project6/app.js
+++ js project/project6/app.js
@@ -1,30 +1,41 @@
 const giturl = "https://api.github.com/users/";
 
-
-const input = document.querySelector("#search")
-
-
+const input = document.querySelector("#search");
 
 const geturl = async (username) => {
-  const Response = await fetch(giturl+username);
+  const Response = await fetch(giturl + username);
   const data = await Response.json();
   console.log(data);
   showdata(data);
 };
 // geturl("DujalSawarkar");
 
-const showdata = (item) =>{
-  const maindiv = document.querySelector("#main")
-  const boxdiv = document.createElement("div")
-  boxdiv.classList.add("box")
-  maindiv.appendChild(boxdiv)
-  console.log("done")
+const showdata = (item) => {
+  const maindiv = document.querySelector("#main");
+  const boxdiv = document.createElement("div");
+  boxdiv.classList.add("box");
+  boxdiv.innerHTML = ` 
+  <div>
+  <img src="${item.avatar_url}" alt="" id="pic" />
+</div>
+<div class="content">
+  <h2>${item.name}</h2>
+  <p>${item.bio}</p>
+  <ul>
+            <li>followers</li>
+            <li>following</li>
+            <li>repo</li>
+          </ul>
+  <ul>
+    <li>${item.followers}</li>
+    <li>${item.following}</li>
+    <li>${item.public_repos}</li>
+  </ul> `;
+ 
+  maindiv.appendChild(boxdiv);
+  console.log("done");
+};
 
-}
-
-input.addEventListener(
-  "focusout",
-  function(){
-    geturl(input.value)
-  }
-)
+input.addEventListener("focusout", function () {
+  geturl(input.value);
+});
