4ff9c494ef2f04b6fda069ce291047f79da17d0a
DujalSawarkar <dujalsawar5@gmail.com>
2023-07-05 00:19:48 +0530

    done with basic UI and some JS

---
+++ js project/project7/app.js
@@ -0,0 +1,14 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`;
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+const weather = document.querySelector("#weatherinfo");
+// const API = `https://api.openweathermap.org/data/2.5/weather?
+// q=${city}&appid=${API_KEY}&units=metric`
+// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const input = document.querySelector("input");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  getdata(input.value);
+  
+});
