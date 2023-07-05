a3efb2634eb70da2a399b41029aad7a3f0eaedf5
DujalSawarkar <dujalsawar5@gmail.com>
2023-07-05 10:28:14 +0530

    done with weather app

--- js project/project7/app.js
+++ js project/project7/app.js
@@ -4,11 +4,39 @@ const search = document.querySelector("#search");
 const weather = document.querySelector("#weatherinfo");
 // const API = `https://api.openweathermap.org/data/2.5/weather?
 // q=${city}&appid=${API_KEY}&units=metric`
-// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 const input = document.querySelector("input");
+
+const getdata = async (city) => {
+  const Response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+  const data = await Response.json();
+  console.log(data);
+  weather.innerHTML = "";
+  //for img
+  // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+  const imgdiv = document.createElement("div");
+  imgdiv.classList.add("image");
+  imgdiv.innerHTML = `<img src="https:openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="" />
+`;
+  weather.appendChild(imgdiv);
+  //info
+  const infodiv = document.createElement("div");
+  infodiv.classList.add("info");
+  infodiv.innerHTML = `
+          <h1>${data.main.temp} °C</h1>
+
+          <h2>${data.name}</h2>
+          <p>
+           ${data.weather[0].description}
+          </p>
+  `;
+  weather.appendChild(infodiv);
+};
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   getdata(input.value);
-  
+  // console.log(input.value);
 });
