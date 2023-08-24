645b2434b72186c8072a11a164276fbe023da9b2
DujalSawarkar <dujalsawar5@gmail.com>
2023-08-24 18:03:19 +0530

    basic of react

---
+++ React-Project/Project-1/src/Score.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+// import { preview } from "vite";
+
+export default function Score({ numPlayers }) {
+  const [score, setScore] = useState(new Array(numPlayers).fill(0));
+  // const [scores, setScores] = useState(0);
+
+  const insNo = (idx) => {
+    setScore((prevScore) => {
+      const copy = [...prevScore];
+      copy[idx] += 1;
+      return copy[idx];
+    });
+  };
+
+  return (
+    <div>
+      <ul>
+        <h1>Heyyy</h1>
+        <ul>
+          {score.map((score, idx) => {
+            return (
+              <li key={idx}>
+                Player{idx + 1} : {score}
+                <button onClick={() => insNo(idx)}>+1</button>
+              </li>
+            );
+          })}
+        </ul>
+      </ul>
+    </div>
+  );
+}
