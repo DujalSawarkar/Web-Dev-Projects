49725c1e1233086b05bd957e924e7420b54520ab
DujalSawarkar <dujalsawar5@gmail.com>
2023-10-04 10:13:20 +0530

    added some file

--- React-Project/t-react-app/src/App.jsx
+++ React-Project/t-react-app/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { filterData , apiUrl} from './data'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+      {console.log(filterData )}
       
     </>
   )
---
+++ React-Project/t-react-app/src/components/Filter.jsx
@@ -0,0 +1,11 @@
+
+export default function Filter(){
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
