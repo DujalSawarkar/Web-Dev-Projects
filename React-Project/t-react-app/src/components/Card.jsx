a91d0c05c0e42152c3979e21c0a769e986e1abd4
DujalSawarkar <dujalsawar5@gmail.com>
2023-10-10 19:17:12 +0530

    added like

--- React-Project/t-react-app/src/components/Card.jsx
+++ React-Project/t-react-app/src/components/Card.jsx
@@ -1,15 +1,19 @@
-
+// import react_svg from "../assets/react.svg";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useState } from "react";
 
 const Card = (props) => {
   let course = props.course;
+  const [isliked, setisliked] = useState(false);
   // console.log(course);
   return (
     <div>
       <div>
         <img src={course.image.url} alt={course.image.alt} />
       </div>
-      <button>
-        
+      <button onClick={() => setisliked((prev) => !prev)}>
+        {isliked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
       </button>
       <div>
         <h1>{course.title}</h1>
