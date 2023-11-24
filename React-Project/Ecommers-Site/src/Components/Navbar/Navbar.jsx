a7efcfa2e3686ae95460ad1f817f03fc2fce6755
DujalSawarkar <dujalsawar5@gmail.com>
2023-11-24 09:06:49 +0530

    ..

--- React-Project/Ecommers-Site/src/Components/Navbar/Navbar.jsx
+++ React-Project/Ecommers-Site/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Assuming you are using React Router for navigation
-import { FaShoppingCart, FaUser } from "react-icons/fa"; // Assuming you have a library like react-icons for icons
+import { Link } from "react-router-dom"; 
+import { FaShoppingCart, FaUser } from "react-icons/fa"; 
 import "./Navbar.css";
 import logo from "../imgs/SHOP.CO.png";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        {/* Replace 'Your Logo' with your actual logo */}
         <img src={logo} alt="Your Logo" />
       </div>
       <div className="nav-links">
@@ -17,17 +16,17 @@ const Navbar = () => {
         <Link to="/new-arrivals">New Arrivals</Link>
       </div>
       <div className="search-bar">
-        {/* Add your search bar component here */}
+       
         <input type="text" placeholder="Search" />
       </div>
       <div className="user-actions">
         <Link to="/cart">
-          <FaShoppingCart /> {/* Cart Icon */}
+          <FaShoppingCart /> 
         </Link>
         <Link to="/login">
-          <FaUser /> {/* User Icon */}
+          <FaUser />
         </Link>
-        {/* You can also add a signup link here */}
+        
       </div>
     </div>
   );
