0312c3b44830ad1f58fc4515ae0e927fb0597474
DujalSawarkar <dujalsawar5@gmail.com>
2024-01-06 20:31:46 +0530

    cart

--- React-Project/Ecommers-Site/frontend/src/Components/Pages/Cart.jsx
+++ React-Project/Ecommers-Site/frontend/src/Components/Pages/Cart.jsx
@@ -1,6 +1,39 @@
 import React from "react";
+import RouteCard from "../routeCard/routeCard";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <RouteCard />
+      <h1>Your cart</h1>
+      <div>
+        <div></div>
+        <div>
+          <p>Order Summary</p>
+          <div>
+            <p>Subtotal</p>
+            <p>$565</p>
+          </div>
+          <div>
+            <p>Discount </p>
+            <p>-$113</p>
+          </div>
+          <div>
+            <p>Delivery Fee</p>
+            <p>$15</p>
+          </div>
+
+          <hr />
+          <div>
+            <p>Total</p>
+            <p>$467</p>
+          </div>
+
+
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Cart;
