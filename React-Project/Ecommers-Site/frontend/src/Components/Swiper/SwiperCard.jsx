e9d16d0d11b8a05ccedc56bec848735053bbc162
DujalSawarkar <dujalsawar5@gmail.com>
2024-02-07 21:51:13 +0530

    ..

--- React-Project/Ecommers-Site/frontend/src/Components/Swiper/SwiperCard.jsx
+++ React-Project/Ecommers-Site/frontend/src/Components/Swiper/SwiperCard.jsx
@@ -1,44 +1,14 @@
-// import React from "react";
-// import "./SwiperCard.css";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination, Navigation } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
+
 import Data2 from "../../Data2.js";
 import SwipeCard from "./SwipeCard.jsx";
-// const SwiperImage = () => {
-//   return (
-//     <div className="image-slider">
-//       <Swiper
-
-//         pagination={{
-//           dynamicBullets: true,
-//         }}
-//         navigation={true}
-//         modules={[Pagination, Navigation]}
-//       >
-//         {Data2.map((e) => (
-//           <SwiperSlide>
-//             <SwipeCard key={e.id} Data2={e} />
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// };
-
-// export default SwiperImage;
 
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import "./styles.css";
 
 // import required modules
 import { Pagination } from "swiper/modules";
