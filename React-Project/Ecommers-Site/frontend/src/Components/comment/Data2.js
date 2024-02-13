21a1a09e2bcc4d5e338717d814e49d1b3e34aa78
DujalSawarkar <dujalsawar5@gmail.com>
2024-02-13 23:19:47 +0530

    ..

---
+++ React-Project/Ecommers-Site/frontend/src/Components/comment/Data2.js
@@ -0,0 +1,52 @@
+const Data2 = [
+  {
+    id: 1,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+  {
+    id: 2,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+  {
+    id: 3,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+  {
+    id: 4,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+  {
+    id: 5,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+  {
+    id: 6,
+    rayte: 4.0,
+    name: "Sarah M.",
+    discription:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+    date:"August 17,2023"
+    },
+];
+
+export default Data2;
